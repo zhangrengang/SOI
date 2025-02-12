3b0b7fb621e862aa57f8d6ed2a4d10b3122cd5e7
zhangrengang <zhangrenang@ori-gene.cn>
2025-02-12 21:40:37 +0800

    fix bug for phylo -only_aln

--- soi/mcscan.py
+++ soi/mcscan.py
@@ -2827,7 +2827,7 @@ class ToAstral(ColinearGroups):
 			cmds += [cmd]
 			iqtree_opts0 = '' #' -o {} '.format(root) if root else ''
 			pep = True
-			if self.cds:
+			if self.cds and not self.onlyaln:
 				iqtree_opts = iqtree_opts0 + ' -mset GTR ' if self.fast else iqtree_opts0 
 				cmd = pal2nal_template.format(pepAln, cdsSeq, cdsAln)
 				cmds += [cmd]
