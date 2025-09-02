89a821e7490d1fd13610e783b463c434ba6f67e8
shang-hongyun <145735990+shang-hongyun@users.noreply.github.com>
2025-09-02 12:07:43 +0800

    add cluster method comp fix1

--- soi/mcscan.py
+++ soi/mcscan.py
@@ -10,7 +10,7 @@ from collections import Counter, OrderedDict
 import itertools
 from Bio import SeqIO, Phylo
 from lazy_property import LazyWritableProperty as lazyproperty
-
+import subprocess
 from .OrthoFinder import catAln, format_id_for_iqtree, lazy_orthologs, \
 	OrthoMCLGroup, OrthoMCLGroupRecord, OrthoFinder, SonicParanoid, \
 	parse_species
@@ -1902,6 +1902,34 @@ def parse_group(groups):
 				xgroup += [group]
 	return xgroup
 
+#shang
+def awk_asort(items):
+    # use Linux awk asort
+    awk_script = '''
+BEGIN {
+    for(i=1; i<ARGC; i++) {
+        arr[i] = ARGV[i]
+    }
+    n = asort(arr)
+    for(i=1; i<=n; i++) {
+        print arr[i]
+    }
+    exit
+}
+'''    
+    process = subprocess.Popen(
+        ['awk', awk_script] + items,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        text=True
+    )
+    
+    output, error = process.communicate()
+    if error:
+        print("Error:", error)
+    return output.strip().split('\n')
+
+
 def cluster_by_mcl(collinearities, orthologs=None, inflation=2,method='mcl',
 				   outgroup=None, ingroup=None, outpre='cluster'):
 	check_cmd('mcl')
@@ -1910,7 +1938,8 @@ def cluster_by_mcl(collinearities, orthologs=None, inflation=2,method='mcl',
 	logger.info('outgroup: {}'.format(outgroup))
 	logger.info('ingroup: {}'.format(ingroup))
 	network = '{}.network'.format(outpre)
-	fout = open(network, 'w')
+	if method=='mcl': 
+		fout = open(network, 'w')
 	np = 0
 	i, j, k = 0, 0, 0
 	G = nx.Graph()
@@ -1925,14 +1954,17 @@ def cluster_by_mcl(collinearities, orthologs=None, inflation=2,method='mcl',
 		if ingroup and not (sp1 in ingroup and sp2 in ingroup):  # only include ingroup
 			k += 1
 			continue
-		G.add_edges_from(rc.pairs)
+		np += len(rc.pairs)
+		if method=='comp': # add_edges for comp and skip write network
+			G.add_edges_from(rc.pairs)
+			continue
 		for g1, g2 in rc.pairs:
-			np += 1
 			line = [g1, g2]
 			if orthologs:
 				line += [str(rc.oi)]
 			fout.write('{}\n'.format('\t'.join(line)))
-	fout.close()
+	if method=='mcl': 
+		fout.close()
 	logger.info(
 		'excluded: {} paralogs, {} in outgroup, {} not in ingroup'.format(i, j, k))
 	cluster = '{}.mcl'.format(outpre)
@@ -1950,9 +1982,8 @@ awk '{{print "SOG"NR": "$0}}' > {output}'''.format(
 					#sorted_genes = awk_asort(list(component))
 					sorted_genes = sorted(list(component))
 					f.write("SOG{}: {}\n".format(i, ' '.join(sorted_genes)))
-		nc = len([1 for line in open(cluster)])
+			nc = i
 	logger.info('{} syntenic gene pairs reslut in {} SOGs'.format(np, nc))
-
 
 def check_cmd(cmd):
 	logger.info('checking `{}`'.format(cmd))
